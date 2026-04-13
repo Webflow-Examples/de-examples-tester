86e9cf8fd50a0acca3c9bd10ddc2bd8262ffc6ba
Tim McMackin <tim.mcmackin@webflow.com>
2026-04-13 12:29:32 -0400

    Heading is Themes

--- src/examples/styles.ts
+++ src/examples/styles.ts
@@ -341,7 +341,7 @@ export const Styles = {
     },
   },
 
-  ThemeAwareness: {
+  Themes: {
     getThemeAndStyles: async () => {
       // Get the current theme
       const theme = await webflow.getTheme()
